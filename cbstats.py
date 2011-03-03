b28ebb63ffd2f1a292a31bf61dadebb6ec98250b
Joar Wandborg <git@wandborg.com>
2011-03-04 00:16:44 +0100

    OMG indentation was all wrong, fixed it\!
    Signed-off-by: Joar Wandborg <git@wandborg.com>

--- cbstats.py
+++ cbstats.py
@@ -38,6 +38,18 @@ class CraftBukkitStats:
             
         return operators
 
+    def get_whitelist(self):
+        ''' Returns a list containing nicknames present in whitelist '''
+        f = open( self.file_server_whitelist, encoding = 'utf-8')
+        lines = f.read().splitlines()
+
+        whitelist = list()
+        
+        for line in lines:
+            whitelist.append( line )
+            
+        return whitelist
+
     def get_properties(self):
         ''' Returns server properties found in the server.properties file '''
         self.update_properties()            
@@ -69,10 +81,9 @@ class CraftBukkitStats:
         
         connected = dict()
         times = list()
-        connected_log = list()
+        self.connected_log = list()
         connected_time_average = float()
         connected_time_per_user = dict()
-        print( f )
         
         for line in lines:
             ''' Search for a line in the server log that matches the "connected" pattern '''
@@ -80,48 +91,48 @@ class CraftBukkitStats:
             if match_connect:
                 groups = match_connect.groups()
                 
-                connected_log.append('{date} {time} {name} connected'.format( debug=groups, date=match_connect.group('date'), time=match_connect.group('time'), name=match_connect.group('user') ) )
+                self.connected_log.append('{date} {time} {name} connected'.format( debug=groups, date=match_connect.group('date'), time=match_connect.group('time'), name=match_connect.group('user') ) )
                 
                 time_connect = time.strptime( match_connect.group('date') + ' ' + match_connect.group('time'), '%Y-%m-%d %H:%M:%S')
                 connected[ match_connect.group('user') ] = time_connect
                 
-                ''' Search for a line in the server log that matches the "disconnected" pattern '''
-                match_disconnect = re.search('^(?P<date>[0-9-]+) (?P<time>[0-9:]+) (.*) (?P<user>\w+) lost connection: disconnect\.(\w+)$', line)
-                if match_disconnect:
-                    time_disconnect = time.strptime( match_disconnect.group('date') + ' ' + match_disconnect.group('time'), '%Y-%m-%d %H:%M:%S')
-                    delta = ( time.mktime( time_disconnect ) -time.mktime(  connected[ match_disconnect.group('user') ] ) )
-                    delta_format = datetime.timedelta( seconds=int(delta) )
-                    connected_log.append('{date} {time} {name} disconnected ({delta})'.format( date=match_disconnect.group('date'), time=match_disconnect.group('time'), name=match_disconnect.group('user'), delta=delta_format ) )
-                    times.append( {'user': match_disconnect.group('user'), 'time': delta } )
-                    
-                    connected_time_total = float()
-                    connected_time_total_discarded = 0
-                    connected_time_per_user = dict()
-                    
-                    for connected_time in times:
-                        ''' Sum up times for all users, filter out really short connects '''
-                        if connected_time['time'] > 120:
-                            connected_time_total += connected_time['time']
-                        else:
-                            connected_time_total_discarded += 1
-                            
-                            ''' Collect total time, segment per user '''
-                            if not connected_time['user'] in connected_time_per_user:
-                                connected_time_per_user[ connected_time['user'] ] = connected_time['time']
-                            else:
-                                connected_time_per_user[ connected_time['user'] ] += connected_time['time']
+            ''' Search for a line in the server log that matches the "disconnected" pattern '''
+            match_disconnect = re.search('^(?P<date>[0-9-]+) (?P<time>[0-9:]+) (.*) (?P<user>\w+) lost connection: disconnect\.(\w+)$', line)
+            if match_disconnect:
+                time_disconnect = time.strptime( match_disconnect.group('date') + ' ' + match_disconnect.group('time'), '%Y-%m-%d %H:%M:%S')
+                delta = ( time.mktime( time_disconnect ) -time.mktime(  connected[ match_disconnect.group('user') ] ) )
+                delta_format = datetime.timedelta( seconds=int(delta) )
+                self.connected_log.append('{date} {time} {name} disconnected ({delta})'.format( date=match_disconnect.group('date'), time=match_disconnect.group('time'), name=match_disconnect.group('user'), delta=delta_format ) )
+                times.append( {'user': match_disconnect.group('user'), 'time': delta } )
+                
+                connected_time_total = float()
+                connected_time_total_discarded = 0
+                connected_time_per_user = dict()
+                
+        for connected_time in times:
+            ''' Sum up times for all users, filter out really short connects '''
+            if connected_time['time'] > 120:
+                connected_time_total += connected_time['time']
+            else:
+                connected_time_total_discarded += 1
+                
+            ''' Collect total time, segment per user '''
+            if not connected_time['user'] in connected_time_per_user:
+                connected_time_per_user[ connected_time['user'] ] = connected_time['time']
+            else:
+                connected_time_per_user[ connected_time['user'] ] += connected_time['time']
                                 
-                    ''' Calculate average logged on time based on overall total logged on time and number of log ons '''
-                    connected_time_average = connected_time_total / ( len( times ) - connected_time_total_discarded )
-                    
-                    ''' Order times per user to be sorted as a high-score list '''
-                    connected_time_per_user = collections.OrderedDict(
-                        sorted(
-                            connected_time_per_user.items(), 
-                            key = lambda t: t[1],
-                            reverse = True
-                            )
-                        )
+        ''' Calculate average logged on time based on overall total logged on time and number of log ons '''
+        connected_time_average = connected_time_total / ( len( times ) - connected_time_total_discarded )
+        
+        ''' Order times per user to be sorted as a high-score list '''
+        connected_time_per_user = collections.OrderedDict(
+            sorted(
+                connected_time_per_user.items(), 
+                key = lambda t: t[1],
+                reverse = True
+                )
+            )
         return {
             'connectedAverage': connected_time_average,
             'timePerUser': connected_time_per_user
@@ -130,8 +141,8 @@ class CraftBukkitStats:
 if __name__ == '__main__':
     stats = CraftBukkitStats()
 
-    print( stats.get_properties() )
-    print( stats.get_property('') )
+#    print( stats.get_properties() )
+#    print( stats.get_property('') )
 
     usageStats = stats.get_usage_stats()
     
