0def7ee0cf5546613dc11bc76e8ceea8cdae12e5
Joar Wandborg <git@wandborg.com>
2011-03-13 23:43:21 +0100

    Added long_description and bumped version
    Signed-off-by: Joar Wandborg <git@wandborg.com>

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 from distutils.core import setup
+import os
+
+if os.path.exists('README.md'):
+    long_description = open('README.md', encoding = 'UTF-8')
+else:
+    long_description = 'See http://github.com/jwandborg/cbstats'
 
 setup(
     name = 'cbstats',
-    version = '1.0.2',
+    version = '1.0.3',
     description = 'CraftBukkit Stats',
+    long_description = long_description,
     author = 'Joar Wandborg',
     author_email = 'python@wandborg.se',
     url = 'https://github.com/jwandborg/cbstats',
