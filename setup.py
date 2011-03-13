deb5a8856c315b0f9164d0d486fd7d9cfa9cf737
Joar Wandborg <git@wandborg.com>
2011-03-14 00:01:20 +0100

    Bumped version to 1.0.4, changed long_description to read from README.rst
    Signed-off-by: Joar Wandborg <git@wandborg.com>

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from distutils.core import setup
 import os
 
-if os.path.exists('README.md'):
-    long_description = open('README.md', encoding = 'UTF-8')
+if os.path.exists('README.rst'):
+    long_description = open('README.rst', encoding = 'UTF-8').read()
 else:
     long_description = 'See http://github.com/jwandborg/cbstats'
 
 setup(
     name = 'cbstats',
-    version = '1.0.3',
+    version = '1.0.4',
     description = 'CraftBukkit Stats',
     long_description = long_description,
     author = 'Joar Wandborg',
@@ -18,6 +18,7 @@ setup(
     packages = ['cbstats'],
     classifiers = [
         'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.0',
         'Topic :: Games/Entertainment',
         'Environment :: Console',
         'Development Status :: 4 - Beta',
