cd93df32ae364951d9d85099ca230b3924a2d048
Joar Wandborg <git@wandborg.com>
2011-03-13 23:14:49 +0100

    setup.py is using find_packages, removed py_modules. This will hopefully fix the cbstats.cbstats problem.
    Signed-off-by: Joar Wandborg <git@wandborg.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 setup(
     name = 'cbstats',
@@ -9,7 +9,8 @@ setup(
     author_email = 'python@wandborg.se',
     url = 'https://github.com/jwandborg/cbstats',
     package_dir = {'cbstats': 'cbstats'},
-    py_modules = ['cbstats'],
+    packages = find_packages(),
+#    py_modules = ['cbstats'],
     classifiers = [
         'Programming Language :: Python :: 3.1',
         'Topic :: Games/Entertainment',
